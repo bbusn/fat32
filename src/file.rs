5813d3cd6ec7dcc44243abb1cb0bb37fd6e3a8d5
bbusn <benoit.busnardo@gmail.com>
2025-12-15 10:28:09 +0100

    Add panic handler

---
+++ src/file.rs
@@ -0,0 +1,3 @@
+pub struct File {
+	size: u32
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+#![no_std]
+
+use core::panic::PanicInfo;
+
 fn main() {
-    println!("Hello, world!");
+    // pass
+}
+
+/* __________ Panic Handler __________ */
+// Needed when doing no_std
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+	// pass
+	loop {}
 }
