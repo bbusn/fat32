07345e0c03009165c0fed09881b05c055a0dfa5e
bbusn <benoit.busnardo@gmail.com>
2025-12-16 11:04:15 +0100

    Add WeeAlloc as global allocator, switch to main entry point as linux already gives a _start

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
 #![no_std]
+#![no_main]
+
+extern crate alloc;
 
 use core::panic::PanicInfo;
+use wee_alloc::WeeAlloc;
 
-fn main() {
-    // pass
+#[global_allocator]
+static ALLOC: WeeAlloc = WeeAlloc::INIT;
+
+/* __________ Main function __________ */
+#[unsafe(no_mangle)]
+pub extern "C" fn main() -> i32 {
+	loop {}
 }
 
 /* __________ Panic Handler __________ */
-// Needed when doing no_std
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-	// pass
 	loop {}
 }
